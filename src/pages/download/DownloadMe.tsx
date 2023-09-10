17f96397c96e56f7b8954f78ce0b45b4f712ef19
Nikita <reichert.nikita@gmail.com>
2023-09-10 15:47:24 +0200

    did app & playstore buttons

--- src/pages/download/DownloadMe.tsx
+++ src/pages/download/DownloadMe.tsx
@@ -1,3 +1,6 @@
+import AppstoreButton from "../../organisms/AppstoreButton";
+import PlaystoreButton from "../../organisms/PlaystoreButton";
+
 function DownloadMe() {
   return (
     <>
@@ -25,20 +28,8 @@ function DownloadMe() {
               <div className="self-stretch text-2xl font-bold leading-10 tracking-widest">
                 Download in den Stores
               </div>
-              <div className="self-stretch justify-start items-start gap-3 inline-flex">
-                <div className="w-[216px] h-[62.40px] justify-center items-center flex">
-                  <div className="w-[216px] h-[62.40px] relative">
-                    <div className="w-[124.05px] h-[10.90px] left-[69.88px] top-[10.23px] absolute"></div>
-                    <div className="w-[127.59px] h-[27.16px] left-[67.86px] top-[28.08px] absolute"></div>
-                  </div>
-                </div>
-                <div className="w-[172.80px] h-[49.92px] justify-center items-center flex">
-                  <div className="w-[172.80px] h-[49.92px] relative">
-                    <div className="w-[29.56px] h-[32.10px] left-[12.76px] top-[8.91px] absolute"></div>
-                    <div className="w-[49.72px] h-[7.82px] left-[52.93px] top-[33.53px] absolute"></div>
-                  </div>
-                </div>
-              </div>
+              <AppstoreButton />
+              <PlaystoreButton />
             </div>
           </div>
         </div>
