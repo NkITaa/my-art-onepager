a97747cdc0b6631d05243a5334819317c2e10ad0
Nikita <reichert.nikita@gmail.com>
2023-09-10 18:13:41 +0200

    appended logo to header

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -4,22 +4,25 @@ import Button from "../organisms/Button";
 function Header() {
   return (
     <>
-      <header className="w-full h-28 px-40 py-6 bg-white justify-center items-center inline-flex">
+      <header className="w-full px-40 py-6 bg-white justify-center items-center inline-flex">
         <div className="grow shrink basis-0 h-16 justify-start items-center flex">
           <div className="grow shrink basis-0 h-16 justify-start items-start flex">
-            <div className="justify-center items-end gap-4 flex">
-              <div className="w-16 h-16 relative" />
-              <div>
-                <Link to="/">
+            <Link to="/">
+              <div className="justify-center items-end gap-4 flex">
+                <img
+                  src="../../assets/logo.svg"
+                  alt="Kiwi standing on oval"
+                ></img>
+                <div>
                   <span className="text-gray-400 text-[32px] font-extrabold leading-[38.40px]">
                     My
                   </span>
                   <span className="text-zinc-800 text-[32px] font-extrabold leading-[38.40px]">
                     Art
                   </span>
-                </Link>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
           <nav>
             <ul className="justify-center items-center gap-12 flex">
