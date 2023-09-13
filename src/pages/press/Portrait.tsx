52e1c36048ac8e37a1ec6aafc4ea01ede03c7603
Nikita <reichert.nikita@gmail.com>
2023-09-13 17:14:44 +0200

    formated press

--- src/pages/press/Portrait.tsx
+++ src/pages/press/Portrait.tsx
@@ -1,42 +1,27 @@
-function Portrait() {
+function Portrait({
+  title,
+  subtitle,
+  content,
+}: {
+  title: string;
+  subtitle: string;
+  content: string[];
+}) {
   return (
     <>
-      <div className="w-full px-40 bg-slate-100 flex-col justify-start items-center inline-flex">
-        <div className="self-stretch py-16 flex-col justify-center items-center gap-16 flex">
-          <div className="self-stretch h-[110px] flex-col justify-start items-start gap-3 flex">
-            <div className="self-stretch text-5xl font-bold leading-[58px]">
-              Kurzportrait
-            </div>
-            <div className="self-stretch text-2xl font-bold leading-10 tracking-widest">
-              Fakten zur Berichterstattung
-            </div>
+      <div className="px-10 lg:px-40 bg-slate-100 flex-col justify-start items-center inline-flex">
+        <div className="py-16">
+          <div className="text-5xl font-bold ">{title}</div>
+          <div className="text-2xl font-bold tracking-widest pt-4 pb-8">
+            {subtitle}
           </div>
-          <div className="self-stretch justify-center items-center inline-flex">
-            <div className="grow shrink basis-0 text-xl  leading-loose">
-              Die MyArt Digital Solutions GmbH ist ein Technologie-Unternehmen
-              im Kunstsektor mit Sitz in Berlin, gegründet im Jahr 2020 von den
-              Schwestern Lena und Sophie Kunstfreund, zusammen mit Marco
-              Bilderlieb und Julia Farbenvoll.
-              <br />
-              <br />
-              Das Unternehmen beschäftigt heute etwa 60 talentierte
-              Mitarbeiterinnen und Mitarbeiter an den Standorten Berlin und
-              Hamburg. Als Gesellschafter des Unternehmens treten die Gründer in
-              Erscheinung, während Minderheitsbeteiligungen von mehreren
-              renommierten Kunstinvestoren gehalten werden. <br />
-              <br />
-              Das Hauptprodukt von MyArt Digital Solutions GmbH ist die App
-              MyArt, die 2021 an den Start ging - eine digitale Plattform, die
-              Kunstliebhabern und Künstlern gleichermaßen dient. Mit MyArt
-              können Benutzer Kunstwerke entdecken, kaufen und verkaufen, sich
-              mit Künstlern direkt austauschen und Teil einer wachsenden
-              Gemeinschaft werden.
-              <br />
-              <br />
-              Die MyArt-App zählt bereits über 800.000 registrierte Nutzer und
-              positioniert sich damit als eine der führenden Plattformen für
-              Kunstinteraktion im deutschen Raum.
-            </div>
+          <div className="text-xl leading-loose">
+            {content.map((paragraph, index) => (
+              <p key={index}>
+                {paragraph} <br />
+                <br />
+              </p>
+            ))}
           </div>
         </div>
       </div>
--- src/pages/press/Press.tsx
+++ src/pages/press/Press.tsx
@@ -3,11 +3,22 @@ import Presskit from "../../universal/Presskit";
 import Portrait from "./Portrait";
 
 function Press() {
+  const content = [
+    "Die MyArt Digital Solutions GmbH ist ein Technologie-Unternehmen im Kunstsektor mit Sitz in Berlin, gegründet im Jahr 2020 von den Schwestern Lena und Sophie Kunstfreund, zusammen mit Marco Bilderlieb und Julia Farbenvoll.",
+    "Das Unternehmen beschäftigt heute etwa 60 talentierte Mitarbeiterinnen und Mitarbeiter an den Standorten Berlin und Hamburg. Als Gesellschafter des Unternehmens treten die Gründer in Erscheinung, während Minderheitsbeteiligungen von mehreren renommierten Kunstinvestoren gehalten werden.",
+    "Das Hauptprodukt von MyArt Digital Solutions GmbH ist die App MyArt, die 2021 an den Start ging - eine digitale Plattform, die Kunstliebhabern und Künstlern gleichermaßen dient. Mit MyArt können Benutzer Kunstwerke entdecken, kaufen und verkaufen, sich mit Künstlern direkt austauschen und Teil einer wachsenden Gemeinschaft werden.",
+    "Die MyArt-App zählt bereits über 800.000 registrierte Nutzer und positioniert sich damit als eine der führenden Plattformen für Kunstinteraktion im deutschen Raum.",
+  ];
+
   return (
     <>
-      <Portrait />
+      <Portrait
+        title="Kurzportrait"
+        subtitle="Fakten zur Berichterstattung"
+        content={content}
+      />
       <Presskit />
-      <Footer />
+      <Footer slate={true} />
     </>
   );
 }
