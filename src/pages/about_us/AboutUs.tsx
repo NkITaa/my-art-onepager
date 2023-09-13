c5045698de424e947b2887a82806050057aa0250
Nikita <reichert.nikita@gmail.com>
2023-09-13 10:11:15 +0200

    made about us responsive

--- src/pages/about_us/AboutUs.tsx
+++ src/pages/about_us/AboutUs.tsx
@@ -5,8 +5,34 @@ import Infos from "./Infos";
 function AboutUs() {
   return (
     <>
-      <Infos align="right" />
-      <Infos align="left" />
+      <Infos
+        align="right"
+        title="unsere Mission"
+        subtitle="wir stellen unsere Mission vor"
+        content="
+              Bei MyArt dreht sich alles um die Kunst und die Menschen dahinter.
+              Unsere Mission ist es, Kunstliebhaber und Künstler auf einer
+              einzigartigen Plattform zusammenzubringen, die sowohl Entdeckung
+              als auch Austausch ermöglicht.
+              Seit unserer Gründung haben wir es uns zur Aufgabe gemacht, eine
+              Brücke zwischen traditionellen Kunstgalerien und der digitalen
+              Welt zu schlagen."
+        image="../../../assets/about_team (2).jpg"
+      />
+      <Infos
+        align="left"
+        title="unsere Mission"
+        subtitle="wir stellen unsere Mission vor"
+        content="
+              Bei MyArt dreht sich alles um die Kunst und die Menschen dahinter.
+              Unsere Mission ist es, Kunstliebhaber und Künstler auf einer
+              einzigartigen Plattform zusammenzubringen, die sowohl Entdeckung
+              als auch Austausch ermöglicht.
+              Seit unserer Gründung haben wir es uns zur Aufgabe gemacht, eine
+              Brücke zwischen traditionellen Kunstgalerien und der digitalen
+              Welt zu schlagen."
+        image="../../../assets/about_team (1).jpg"
+      />
       <Download />
       <Footer />
     </>
--- src/pages/about_us/Infos.tsx
+++ src/pages/about_us/Infos.tsx
@@ -1,42 +1,41 @@
-function Infos({ align }: { align: "left" | "right" }) {
+function Infos({
+  align,
+  title,
+  subtitle,
+  content,
+  image,
+}: {
+  align: "left" | "right";
+  title: string;
+  subtitle: string;
+  content: string;
+  image: string;
+}) {
   return (
     <>
-      <div className="w-full px-40 bg-slate-100 flex-col justify-center items-center inline-flex">
-        <div className="self-stretch py-16 justify-center items-center gap-16 inline-flex">
+      <div
+        className={`${align === "left" ? "bg-white" : "bg-slate-100"} py-16`}
+      >
+        <div className="flex flex-row flex-wrap items-center justify-center">
           {align === "left" ? (
-            <div className="grow shrink basis-0 flex-col justify-start items-start inline-flex">
+            <div className="p-8">
               <img
-                className="w-[668px] h-[400px] relative rounded-2xl"
-                src="https://via.placeholder.com/668x400"
+                className="w-[668px] h-[400px] rounded-2xl object-cover"
+                src={image}
               />
             </div>
           ) : null}
-          <div className="grow shrink basis-0 flex-col justify-center items-start gap-6 inline-flex">
-            <div className="self-stretch h-[110px] flex-col justify-start items-start gap-3 flex">
-              <div className="self-stretch  text-5xl font-bold leading-[58px]">
-                unsere Mission
-              </div>
-              <div className="self-stretch  text-2xl font-bold leading-10 tracking-widest">
-                wir stellen unsere Mission vor
-              </div>
-            </div>
-            <div className="self-stretch  text-xl  leading-loose">
-              Bei MyArt dreht sich alles um die Kunst und die Menschen dahinter.
-              Unsere Mission ist es, Kunstliebhaber und Künstler auf einer
-              einzigartigen Plattform zusammenzubringen, die sowohl Entdeckung
-              als auch Austausch ermöglicht. <br />
-              <br />
-              Seit unserer Gründung haben wir es uns zur Aufgabe gemacht, eine
-              Brücke zwischen traditionellen Kunstgalerien und der digitalen
-              Welt zu schlagen.{" "}
-            </div>
+          <div className="max-w-[668px] p-8">
+            <div className="text-5xl font-bold pb-2">{title}</div>
+            <div className="text-2xl font-bold tracking-widest">{subtitle}</div>
+            <div className="text-xl pt-6">{content}</div>
           </div>
 
           {align === "right" ? (
-            <div className="grow shrink basis-0 flex-col justify-start items-start inline-flex">
+            <div className="p-8">
               <img
-                className="w-[668px] h-[400px] relative rounded-2xl"
-                src="https://via.placeholder.com/668x400"
+                className="w-[668px] h-[400px] rounded-2xl object-cover"
+                src={image}
               />
             </div>
           ) : null}
