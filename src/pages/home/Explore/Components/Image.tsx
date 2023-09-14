1df6fd41b8e31eccde003ee1a01200e689f8acde
Nikita <reichert.nikita@gmail.com>
2023-09-14 14:06:26 +0200

    smaller pics & fixed caroussel

--- src/pages/home/Explore/Components/Image.tsx
+++ src/pages/home/Explore/Components/Image.tsx
@@ -1,16 +1,15 @@
 function Image({ url, mockup }: { url: string; mockup: string }) {
   return (
     <>
-      <div className="relative z-0">
+      <div className="flex flex-col align-middle relative">
         <img
-          className="naked rounded-[32.40px] border border-black border-opacity-50"
+          className="naked rounded-[32.40px] h-full absolute  border border-black border-opacity-50"
           src={url}
         />
         <img
-          className="frame rounded-[32.40px] transition-transform absolute inset-0 flex opacity-0  justify-center items-center  z-10"
+          className="frame transition-transform relative opacity-0 z-10"
           src={mockup}
         />
-        <div className=""></div>
       </div>
     </>
   );
