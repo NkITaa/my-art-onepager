e928e0a6eee5e6fd67f87eaa2ed78c7729111cf0
Nikita <reichert.nikita@gmail.com>
2023-09-17 16:07:11 +0200

    test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,14 @@
 import Download from "../../universal/Download";
 import Footer from "../../universal/footer/Footer";
-import Banner from "./banner/Banner";
 import Explore from "./explore/Explore";
+import Infos from "./infos/Infos";
+import Referals from "./referals/Referals";
+import info1 from "../../assets/artwork_mockup frontal.png";
+import mockup1 from "../../assets/carousel/mockup_1.png";
+import info2 from "../../assets/artwork_mockup diagonal right.png";
+import mockup5 from "../../assets/carousel/mockup_5.png";
+import info3 from "../../assets/artwork_mockup diagonal left.png";
+import mockup4 from "../../assets/carousel/mockup_4.png";
 
 function Home() {
   const arr: string[] = [
@@ -11,8 +18,47 @@ function Home() {
   ];
   return (
     <>
-      <Banner />
+      <Infos
+        align="left"
+        image={info1}
+        imageSmall={mockup1}
+        title="Kunstmarkplatz"
+        subtitle="kaufe und verkaufe deine Kunst"
+        bullets={arr}
+        buttonContent="Durchstöbere eine umfangreiche Sammlung von Werken.
+        MyArt ermöglicht es dir, Kunstwerke direkt von talentierten
+        Künstlern. Erwerben oder selbst zum Verkauf anzubieten, wobei
+        der gesamte Prozess sicher."
+        buttonTitle="mehr Informationen"
+      />
+      <Infos
+        align="right"
+        image={info2}
+        imageSmall={mockup5}
+        title="Kunstmarkplatz"
+        subtitle="kaufe und verkaufe deine Kunst"
+        bullets={arr}
+        buttonContent="Durchstöbere eine umfangreiche Sammlung von Werken.
+                  MyArt ermöglicht es dir, Kunstwerke direkt von talentierten
+                  Künstlern. Erwerben oder selbst zum Verkauf anzubieten, wobei
+                  der gesamte Prozess sicher."
+        buttonTitle="mehr Informationen"
+      />
+      <Infos
+        align="left"
+        image={info3}
+        imageSmall={mockup4}
+        title="Kunstmarkplatz"
+        subtitle="kaufe und verkaufe deine Kunst"
+        bullets={arr}
+        buttonContent="Durchstöbere eine umfangreiche Sammlung von Werken.
+                  MyArt ermöglicht es dir, Kunstwerke direkt von talentierten
+                  Künstlern. Erwerben oder selbst zum Verkauf anzubieten, wobei
+                  der gesamte Prozess sicher."
+        buttonTitle="mehr Informationen"
+      />
       <Explore />
+      <Referals />
       <Download />
       <Footer />
     </>
