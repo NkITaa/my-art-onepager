b8bcc4e073d65531fdceb4076b81f3c02377f249
nreicher <nikita.reichert@capgemini.com>
2023-09-21 12:38:05 +0200

    removed delay

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -10,7 +10,6 @@ import NotFound from "./404/NotFound";
 import LegalTemplate from "./legal_template/LegalTemplate";
 import Cookies from "../universal/organisms/Cookies";
 import { useEffect, useState } from "react";
-import { delay } from "framer-motion";
 
 function App() {
   const [visible, setVisible] = useState(false);
@@ -33,9 +32,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         setArt(data.artObjects[0].webImage.url);
-        delay(function () {
-          setLoading(false);
-        }, 1000);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
